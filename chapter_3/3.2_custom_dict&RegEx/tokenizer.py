ba3cc0d17d587f68b029b489d34ef4945fc93cd4
HY <iyongyong@126.com>
2019-04-27 18:10:41 +0800

    添加词性处理部分

--- chapter_3/3.2_custom_dict&RegEx/tokenizer.py
+++ chapter_3/3.2_custom_dict&RegEx/tokenizer.py
@@ -10,6 +10,17 @@ startJVM(getDefaultJVMPath(), djclass_path, "-Xms1g", "-Xmx1g")
 
 Tokenizer = JClass('com.hankcs.hanlp.tokenizer.StandardTokenizer')  # 标准分词器
 
+keep_pos = "q,qg,qt,qv,s,t,tg,g,gb,gbc,gc,gg,gm,gp,m,mg,Mg,mq,n,an,vn,ude1,nr,ns,nt,nz,nb,nba,nbc,nbp,nf,ng,nh,nhd,o,nz,nx,ntu,nts,nto,nth,ntch,ntcf,ntcb,ntc,nt,nsf,ns,nrj,nrf,nr2,nr1,nr,nnt,nnd,nn,nmc,nm,nl,nit,nis,nic,ni,nhm,nhd"
+keep_pos_nouns = set(keep_pos.split(","))
+keep_pos_v = "v,vd,vg,vf,vl,vshi,vyou,vx,vi"
+keep_pos_v = set(keep_pos_v.split(","))
+keep_pos_p = set(['p', 'pbei', 'pba'])
+drop_pos_set = set(
+    ['xu', 'xx', 'y', 'yg', 'wh', 'wky', 'wkz', 'wp', 'ws', 'wyy', 'wyz', 'wb', 'u', 'ud', 'ude1', 'ude2', 'ude3',
+     'udeng', 'udh', 'p', 'rr', 'w'])
+han_pattern = re.compile(r'[^\dA-Za-z\u3007\u4E00-\u9FCB\uE815-\uE864]+')
+HanLP = JClass('com.hankcs.hanlp.HanLP')
+
 
 def to_string(sentence, return_generator=False):
     if return_generator:
