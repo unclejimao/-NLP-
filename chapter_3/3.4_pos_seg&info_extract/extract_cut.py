50f5bd0449ddcbaf880b36dabb571d76e75bdaca
HY <iyongyong@126.com>
2019-04-27 18:46:09 +0800

    添加相关注释

--- chapter_3/3.4_pos_seg&info_extract/extract_cut.py
+++ chapter_3/3.4_pos_seg&info_extract/extract_cut.py
@@ -3,13 +3,13 @@
 
 import jieba
 import re
-from tokenizer import seg_sentences
+from tokenizer import seg_sentences  # 使用Hanlp的切分工具
 
 fp = open("text.txt", 'r', encoding='utf8')
 fout = open("out.txt", 'w', encoding='utf8')
 for line in fp:
-    line = line.strip()
-    if len(line) > 0:
+    line = line.strip()  # 丢弃行末换行符
+    if len(line) > 0:  # 对非空行进行处理
         fout.write(' '.join(seg_sentences(line)) + "\n")
 fout.close()
 if __name__ == "__main__":
