476cac03898af1632ffa5b7cb0f86e31b613c40e
HY <iyongyong@126.com>
2019-04-28 12:30:26 +0800

    使用StanfordCoreNLP进行命名实体识别

---
+++ chapter_3/3.5_named_entity_recognization/ner.py
@@ -0,0 +1,13 @@
+# -*- encoding:utf-8 -*-
+# author: unclejimao
+
+import jieba
+import re
+from grammer.rules import grammer_parse
+
+with open('text.txt', 'r', encoding='utf8') as fp:
+    with open('out.txt', 'w', encoding='utf8') as fout:
+        [grammer_parse(line.strip(), fout) for line in fp if len(line.strip()) > 0]
+
+if __name__ == '__main__':
+    pass
