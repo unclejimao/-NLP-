e4c5b18dbc7c40831938d0378cb51f3954ec871c
HY <iyongyong@126.com>
2019-04-30 21:52:01 +0800

    使用jieba提供的TextRank算法实现文本关键词抽取

---
+++ chapter_3/3.6_TextRank/text_rank.py
@@ -0,0 +1,12 @@
+# -*- encoding:utf-8 -*-
+# author: unclejimao
+
+from jieba import analyse
+
+textrank = analyse.textrank  # jieba的TextRank关键词抽取接口
+text = "非常线程是程序执行时的最小单位，它是进程的一个执行流，\ 是CPU调度和分派的基本单位，一个进程可以由很多个线程组成，\ 线程间共享进程的所有资源，每个线程有自己的堆栈和局部变量。\ 线程由CPU独立调度执行，在多CPU环境下就允许多个线程同时运行。\ 同样多线程也可以实现并发操作，每个请求分配一个线程来处理。"
+print('\nkeywords by textrank:')
+keywords = textrank(text, topK=10, withWeight=True, allowPOS=('n', 'ns'))
+
+words = [keyword for keyword, w in keywords if w > 0.2]
+print(' '.join(words) + '\n')
